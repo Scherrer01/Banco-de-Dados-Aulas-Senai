fa239ada01a535b3b63078a514ae3d69492320ef
Scherrer01 <diogo.scherrer@gmail.com>
2025-08-08 16:36:21 -0300

    08.08.25

---
+++ Aula 01/Bando de Dados Pizzaria.sql
@@ -0,0 +1,15 @@
+create database Pizzaria;
+
+use Pizzaria;
+
+create table Cardapio (
+ID_Pizza int primary key
+);
+
+create table Funcionario (
+ID_Funcionario int primary key
+);
+
+create table Delivery (
+ID_Entrega int primary key
+);
---
+++ Aula 01/exemplo script modelo Escola.sql
@@ -0,0 +1,15 @@
+create database exemplo;
+
+use exemplo;
+
+create table aluno (
+id_aluno int primary key
+);
+
+create table funcionarios (
+id_funcionario int primary key
+);
+
+create table cursos (
+id_curso int primary key
+);
