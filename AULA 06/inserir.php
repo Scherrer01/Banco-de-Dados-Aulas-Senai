cdda09659ad309610831aeeb47834c2ac7fcdc3a
Scherrer01 <diogo.sherrer@gmail.com>
2025-10-31 16:52:59 -0300

    index-php-sql

---
+++ AULA 06/inserir.php
@@ -0,0 +1,28 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+
+$conn = new mysqli(
+         "localhost",
+         "root",
+         "senaisp",
+         "livraria"
+);
+
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
+if ($conn->query($sql) === TRUE) {
+    echo "Dados salvos com sucesso!";
+} else {
+    echo "Erro: " . $conn->error;
+} 
+
+$conn->close();
+header("Location: index.html"); //ou index.php, depensendo do seu arquivo principal
+exit;
+
+
+?>
